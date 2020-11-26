977ae27ce197d7919db6bcc0cb8cd0e5556123ed
Euijin Yang <trustnloveu@gmail.com>
2020-11-26 23:58:55 +0900

    Set environment for preparing integration test.

--- startup/db.js
+++ startup/db.js
@@ -1,14 +1,17 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function (winston) {
+  const db = config.get("db");
   // connecting to DB(Only once when the propject starts)
+  // "mongodb://localhost/movieRentalService"
   mongoose
-    .connect("mongodb://localhost/movieRentalService", {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     })
-    .then(() => winston.info("Connected to MongoDB..."));
+    .then(() => winston.info(`Connected to ${db}...`));
   // .catch((err) => console.log("It could not connect to MongoDB...", err));
   // the catch method will be handled by global error handleing module
 };
