07210c30045f92b101a555ea98a7c3d8bbb76282
Euijin Yang <trustnloveu@gmail.com>
2020-11-28 13:15:51 +0900

    Test genre rest GET & POST requests, Refactor again

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -3,16 +3,23 @@
 // So, you should load the server before, and close it after each test
 const request = require("supertest");
 const { Genre } = require("../../models/gerne");
+const { User } = require("../../models/user");
+
 let server;
 
 describe("/api/genres", () => {
+  // before > call the server
   beforeEach(() => {
     server = require("../../index");
   });
+
+  // after > clean the server
   afterEach(async () => {
     server.close();
     await Genre.deleteMany({});
   });
+
+  // Get
   describe("GET /", () => {
     it("should return all genres", async () => {
       await Genre.collection.insertMany([
@@ -29,6 +36,7 @@ describe("/api/genres", () => {
     });
   });
 
+  // Get with id
   describe("GET /:id", () => {
     it("should return a genre if valid id is passed", async () => {
       const genre = new Genre({ name: "genre1" });
@@ -44,6 +52,62 @@ describe("/api/genres", () => {
       const res = await request(server).get("/api/genres/1");
 
       expect(res.status).toBe(404);
+    });
+  });
+
+  // Post
+  describe("POST /", () => {
+    // initialize
+    let token;
+    let name;
+
+    // response
+    const exec = async () => {
+      return await request(server)
+        .post("/api/genres")
+        .set("x-auth-token", token)
+        .send({ name: name });
+    };
+
+    // before
+    beforeEach(() => {
+      token = new User().generateAuthToken();
+      name = "genre1";
+    });
+
+    it("should return 401 if client is not logged in", async () => {
+      token = "";
+      const res = await exec();
+
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is invalid - less then 3 characters", async () => {
+      name = "12";
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should return 400 if genre is invalid - more then 50 characters", async () => {
+      name = new Array(52).join("a");
+      const res = await exec();
+
+      expect(res.status).toBe(400);
+    });
+
+    it("should save the genre if it is valid", async () => {
+      await exec();
+
+      const genre = await Genre.find({ name: "genre1" });
+      expect(genre).not.toBeNull();
+    });
+
+    it("should return the genre if genre is valid", async () => {
+      res = await exec();
+
+      expect(res.body).toHaveProperty("_id");
+      expect(res.body).toHaveProperty("name", "genre1");
     });
   });
 });
